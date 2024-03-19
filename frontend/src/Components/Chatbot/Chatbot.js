1802a556b9391822fad1ad3f489b6fa6a8058620
fridahnamudu <fridah.namudu@gmail.com>
2024-03-19 22:07:12 +0300

    Add the chatbot

---
+++ frontend/src/Components/Chatbot/Chatbot.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import './Chatbot.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+
+const API_KEY = 'AIzaSyDNlOAlRcMxxL0JU5YR-KqMaGenYpMDu6w'
+
+
+function Chatbot() {
+    const [typing, setTyping] = useState(false);
+
+    const [messages, setMessages] = useState([
+        {
+            message: "Hello there I am Gemini AI",
+            sender: "Gemini"
+        }
+    ]);
+
+    const handleSend = async (message) => {
+        const newMessage = {
+            message: message,
+            sender: 'user',
+            direction: 'outgoing'
+        };
+
+        const newMessages = [...messages, newMessage]; //all new messages + old messages
+        // update our messages state
+        setMessages(newMessages);
+        // typing messages indictator
+        setTyping(true);
+
+        await processMessageToGemini(newMessages);
+
+    };
+    async function processMessageToGemini(chatMessages) {
+
+        let apiMessages = chatMessages.map((messageObject) => {
+            let role = 'http://localhost:3500/api/chat';
+            if(messageObject.sender === 'Gemini') {
+                role = "assistant"
+            } else {
+                role = "user" 
+            }
+            return { role: role, content:messageObject.message }
+            
+        });
+
+        // how we want our gemini to talk
+        const systemMessage = {
+           role: "system",
+           content: "Explain laying emphasis on Kenya" 
+        }
+
+        const apiRequestBody = {
+            "model": "",
+            "messages" : [ systemMessage, ...apiMessages]
+        }
+        await fetch('', {
+            method: 'POST',
+            headers: {
+                "Authorization": "Bearer" + API_KEY,
+                "Content-Type": "application/json"
+
+            },
+            body: JSON.stringify(apiRequestBody)
+        }).then((data) => {
+            return data.json();
+
+        }).then((data) => {
+            console.log(data)
+        })
+
+    }
+
+    return (
+        <div className='Chatbot'>
+            <div>
+                <MainContainer>
+                    <ChatContainer>
+                        <MessageList typingIndicator={typing ? <TypingIndicator content="Gemini is typing"/> : null}
+                        >
+                            {messages.map((message, i) => {
+                                return <Message key={i} model={message} />
+                            })}
+                        </MessageList>
+                        <MessageInput placeholder='Type Message Here' onSend={handleSend} />
+                    </ChatContainer>
+                </MainContainer>
+            </div>
+        </div>
+    );
+}
+
+export default Chatbot;
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import Chatbot from '../Components/Chatbot/Chatbot'
 
 function Home() {
   return (
     <div>
         <Hero />
+        <Chatbot />
         
     </div>
   )
