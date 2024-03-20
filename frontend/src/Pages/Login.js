b6701f7877d5871fb3482fe1d1bd901521e4cce2
codeitownit <gachuieve32@gmail.com>
2024-03-19 17:34:23 -0700

    implemented user login

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../css/Login.css'
+import axios from 'axios';
 
 function Login() {
-   
+  const[email, setEmail] = useState("")
+  const[password, setPassword] = useState("")
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3500/api/auth/login', {email, password});
+      console.log('Login successful:', response.data);
+    } catch (err) {
+      console.log(err)
+    }
+  };
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
         <h1>Log In</h1>
+        <form onSubmit={handleLogin}>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
-
-
+          <input type="email" placeholder='Email Address' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+          <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
         </div>
-        <button> Continue </button>
+        <button type='submit'> Continue </button>
         <p className='loginsignup-login'> Don't have an account? <span> Sign Up</span></p>
         <div className="loginsignup-agree">
           <input type="checkbox" name='' id='' />
           <p>By continuing, I agree to the terms of use & privacy policy</p>
         </div>
+        </form>
       </div>
     </div>
   )
